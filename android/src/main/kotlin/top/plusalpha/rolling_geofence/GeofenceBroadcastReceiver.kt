db71c5f1fffea7b5af3160d7daf19804c534552f
hummingengineer <hummingengineer@gmail.com>
2024-01-27 02:05:53 +0900

    테스트

--- android/src/main/kotlin/top/plusalpha/rolling_geofence/GeofenceBroadcastReceiver.kt
+++ android/src/main/kotlin/top/plusalpha/rolling_geofence/GeofenceBroadcastReceiver.kt
@@ -1,12 +1,16 @@
 package top.plusalpha.rolling_geofence
 
+import android.app.PendingIntent
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.google.android.gms.location.Geofence
 import com.google.android.gms.location.GeofenceStatusCodes
+import com.google.android.gms.location.GeofencingClient
 import com.google.android.gms.location.GeofencingEvent
+import com.google.android.gms.location.GeofencingRequest
+import com.google.android.gms.location.LocationServices
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.embedding.engine.dart.DartExecutor
 
@@ -55,6 +59,44 @@ class GeofenceBroadcastReceiver : BroadcastReceiver() {
             val requestIdList = triggeringGeofences?.map { it.requestId }.orEmpty()
             val args = mutableListOf(geofenceTransitionStr)
             args.addAll(requestIdList)
+
+
+
+
+            if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_EXIT) {
+                val geofenceList = mutableListOf<Geofence>()
+                geofenceList.add(
+                    Geofence.Builder()
+                        .setRequestId("test123")
+                        .setCircularRegion(
+                            37.7132,
+                            126.8901,
+                            350.0f,
+                            )
+                        .setExpirationDuration(Geofence.NEVER_EXPIRE)
+                        .setLoiteringDelay(5000)
+                        .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_ENTER or Geofence.GEOFENCE_TRANSITION_EXIT or Geofence.GEOFENCE_TRANSITION_DWELL)
+                        .setNotificationResponsiveness(5000)
+                        .build())
+                val geofencePendingIntent = PendingIntent.getBroadcast(context, 2345, intent, PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_MUTABLE)
+                val gClient = LocationServices.getGeofencingClient(context)
+                gClient.removeGeofences(geofencePendingIntent)
+                gClient.addGeofences(GeofencingRequest.Builder().apply {
+                    setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_ENTER)
+                    addGeofences(geofenceList)
+                }.build(), geofencePendingIntent).run {
+                    addOnSuccessListener {
+                        //
+                    }
+                    addOnFailureListener {
+                        //
+                    }
+                }
+            }
+
+
+
+
             engine.dartExecutor.executeDartEntrypoint(DartExecutor.DartEntrypoint("lib/main.dart", "onGeofenceEvent"), args)
         } else {
             // Log the error.
