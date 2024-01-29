2fb75815c8bf0d2d87f6e3a3b5a6b049ac9f77cc
hummingengineer <hummingengineer@gmail.com>
2024-01-30 01:52:43 +0900

    Revert "startSingleLocationRequest 전달인자 수정"
    This reverts commit 46d82fd58fb4c416c3cb6fb446be75e3a15d200d.

--- android/src/main/kotlin/top/plusalpha/rolling_geofence/RollingGeofencePlugin.kt
+++ android/src/main/kotlin/top/plusalpha/rolling_geofence/RollingGeofencePlugin.kt
@@ -149,7 +149,7 @@ class RollingGeofencePlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
                 }
 
                 // result는 함수 내에서 비동기적으로 처리한다.
-                startSingleLocationRequest(applicationContext!!, binding!!.activity, result)
+                startSingleLocationRequest(applicationContext!!, result)
             }
 
             else -> {
@@ -381,7 +381,7 @@ class RollingGeofencePlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
         }
     }
 
-    private fun startSingleLocationRequest(context: Context, activity: Activity, result: Result) {
+    private fun startSingleLocationRequest(context: Context, result: Result) {
         if (fusedLocationClient == null) {
             Log.e("Geofence", "Fused location client is not ready")
             result.error("FusedLocationClientNotReady", null, null)
@@ -428,7 +428,7 @@ class RollingGeofencePlugin : FlutterPlugin, MethodCallHandler, ActivityAware,
             if (exception is ResolvableApiException) {
                 try {
                     exception.startResolutionForResult(
-                        activity,
+                        (context as Activity)!!,
                         3000
                     )
                 } catch (sendEx: IntentSender.SendIntentException) {
